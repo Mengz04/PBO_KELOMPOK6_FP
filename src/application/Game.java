866d0bcbf82ebc198ab2346f3bdff1716fc9802d
Muhammad Rafi Insan Fillah <78022264+Mengz04@users.noreply.github.com>
2022-12-11 08:12:53 +0700

    Update Game.java

--- src/application/Game.java
+++ src/application/Game.java
@@ -1,1 +1,22 @@
+package application;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			GameWindow window = new GameWindow();
+			primaryStage = window.getMainStage();
+			primaryStage.setTitle("Zombie Game");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
