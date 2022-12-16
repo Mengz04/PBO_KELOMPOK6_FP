963e5dc11add4e7fd57f53c6e495b03caf0a4c75
Muhammad Rafi Insan Fillah <78022264+Mengz04@users.noreply.github.com>
2022-12-16 22:04:23 +0700

    Update Mob.java

--- src/application/Mob.java
+++ src/application/Mob.java
@@ -38,72 +38,79 @@ public class Mob extends GameObject {
 	}
 	
 	public void findPlayer() {
-		for(int i=0; i<handler.object.size(); i++) {
-			if(handler.object.get(i).getId() == ID.Player) {
-				tempPlayer = handler.object.get(i);
-				break;
+			for(int i=0; i<handler.object.size(); i++) {
+				if(handler.object.get(i).getId() == ID.Player) {
+					tempPlayer = handler.object.get(i);
+					break;
+				}
 			}
-		}
 	}
 	
 	private void collide() {
-		hit = false;
-		for(int i=0; i<handler.object.size(); i++) {
-			if(handler.object.get(i).getId() == ID.Weapon) {
-				tempCollide = handler.object.get(i);
-				if(getBounds().getBoundsInParent().intersects(tempCollide.getBounds().getBoundsInParent())){
-					this.HP -= tempCollide.getDamage();
-					hit = true;
+			hit = false;
+			for(int i=0; i<handler.object.size(); i++) {
+				if(handler.object.get(i).getId() == ID.Weapon || handler.object.get(i).getId() == ID.Bullet) {
+					tempCollide = handler.object.get(i);
+					if(getBounds().getBoundsInParent().intersects(tempCollide.getBounds().getBoundsInParent())){
+						this.HP -= tempCollide.getDamage();
+						hit = true;
+					}
+				}
+				else if(handler.object.get(i).getId() == ID.Grenade) {
+					tempCollide = handler.object.get(i);
+					if(getBounds().getBoundsInParent().intersects(tempCollide.getBounds().getBoundsInParent())){
+						tempCollide.HP =0;
+					}
 				}
 			}
-		}
 	}
 	
 	@Override
 	public void move() {
-		if(HP<=0 || despawnTimer <=0) { //mati
-			hitBrightness.setBrightness(0);
-			mobIcon.setEffect(hitBrightness);
-			mobIcon.setImage(deadMobIcon);
-			if(deathDuration>0) deathDuration--;
-			else{
-				gamePane.getChildren().remove(mobIcon);
-				handler.addRemoveTask(this);
-			}
-		}
-		else { //hidup
-			collide();
-			
-			if(hit) {
-				hitBrightness.setBrightness(0.7);
-				mobIcon.setEffect(hitBrightness);
-			}
-			else {
+			if(HP<=0 || despawnTimer <=0) { //mati
 				hitBrightness.setBrightness(0);
 				mobIcon.setEffect(hitBrightness);
+				mobIcon.setImage(deadMobIcon);
+				if(deathDuration>0) deathDuration--;
+				else{
+					gamePane.getChildren().remove(mobIcon);
+					handler.addRemoveTask(this);
+				}
 			}
-			if(tempPlayer.x-this.x >0) { //kanan
-				mobIcon.setImage(rightMobIcon);
-				mobIcon.setLayoutX(mobIcon.getLayoutX() + 0.6);
-				this.x += 0.6;
+			else { //hidup
+				if(tempPlayer.x-this.x >0) { //kanan
+					mobIcon.setImage(rightMobIcon);
+					mobIcon.setLayoutX(mobIcon.getLayoutX() + 0.6);
+					this.x += 0.6;
+				}
+				if(tempPlayer.y-this.y >0) { //bawah
+					mobIcon.setImage(rightMobIcon);
+					mobIcon.setLayoutY(mobIcon.getLayoutY() + 0.6);
+					this.y += 0.6;
+				}
+				if(tempPlayer.y-this.y <0) { //atas
+					mobIcon.setImage(rightMobIcon);
+					mobIcon.setLayoutY(mobIcon.getLayoutY() - 0.6);
+					this.y -= 0.6;
+				}
+				if(tempPlayer.x-this.x <0) { //kiri
+					mobIcon.setImage(leftMobIcon);
+					mobIcon.setLayoutX(mobIcon.getLayoutX() - 0.6);
+					this.x -= 0.6;
+				}
+				collide();
+				if(this != null) {
+					if(hit) {
+						hitBrightness.setBrightness(0.7);
+						mobIcon.setEffect(hitBrightness);
+					}
+					else {
+						hitBrightness.setBrightness(0);
+						mobIcon.setEffect(hitBrightness);
+					}
+				}
 			}
-			if(tempPlayer.y-this.y >0) { //bawah
-				mobIcon.setImage(rightMobIcon);
-				mobIcon.setLayoutY(mobIcon.getLayoutY() + 0.6);
-				this.y += 0.6;
-			}
-			if(tempPlayer.y-this.y <0) { //atas
-				mobIcon.setImage(rightMobIcon);
-				mobIcon.setLayoutY(mobIcon.getLayoutY() - 0.6);
-				this.y -= 0.6;
-			}
-			if(tempPlayer.x-this.x <0) { //kiri
-				mobIcon.setImage(leftMobIcon);
-				mobIcon.setLayoutX(mobIcon.getLayoutX() - 0.6);
-				this.x -= 0.6;
-			}
-		}
-		despawnTimer--;
+			despawnTimer--;
 	}
 	
 	public Rectangle getBounds() {
