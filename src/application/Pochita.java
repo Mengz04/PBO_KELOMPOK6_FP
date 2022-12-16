c7188a77df7f8368af3f0f949e975ed791ba3ad6
Muhammad Rafi Insan Fillah <78022264+Mengz04@users.noreply.github.com>
2022-12-16 14:26:55 +0700

    Update Pochita.java

--- src/application/Pochita.java
+++ src/application/Pochita.java
@@ -7,7 +7,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
 
 public class Pochita extends GameObject{
-	//GIF assets
 	private ImageView pochitaIcon = new ImageView 
 			("/resources/pochita-right.gif");
 	private Image rightTopPochitaIcon = new Image 
@@ -18,7 +17,6 @@ public class Pochita extends GameObject{
 			("/resources/pochita-left.gif");
 	private Image leftBotPochitaIcon = new Image 
 			("/resources/pochita-leftDown.gif");
-	
 	private Handler handler;
 	private AnchorPane gamePane;
 	private GameObject tempPlayer=null;
@@ -27,7 +25,7 @@ public class Pochita extends GameObject{
 	private int time = 50;
 	
 	public Pochita(float x, float y, ID id, Handler handler, AnchorPane gamePane, String orientation) {
-		super(x, y, 0, 2, 90, 90, id);
+		super(x, y, 0, 2, 0, 90, 90, id);
 		this.handler = handler;
 		this.id = id;
 		this.gamePane = gamePane;
@@ -36,10 +34,9 @@ public class Pochita extends GameObject{
 		findPlayer();
 	}
 
-	public void findPlayer() { //cari player
+	public void findPlayer() {
 		for(int i=0; i<handler.object.size(); i++) {
 			if(handler.object.get(i).getId() == ID.Player) {
-				
 				tempPlayer = handler.object.get(i);
 				break;
 			}
@@ -47,7 +44,7 @@ public class Pochita extends GameObject{
 	}
 	
 	@Override
-	public void move() { //translasi bergantung pada orientasi NA (normal attack)
+	public void move() {
 		if(orientation == "top-right") {
 			pochitaIcon.setImage(rightTopPochitaIcon);
 			pochitaIcon.setLayoutX(tempPlayer.getX()+50);
@@ -80,19 +77,19 @@ public class Pochita extends GameObject{
 			pochitaIcon.setLayoutY(tempPlayer.getY()+50);
 			y = tempPlayer.getY()+50;
 		}
-		time -=1; //deplete NA duration
-		if(time <= 0) { //jika duration habis delete object
+		time -=1;
+		if(time <= 0) {
 			gamePane.getChildren().remove(pochitaIcon);
 			handler.removeObject(this);
 		}
 	}
 
 	@Override
-	public Rectangle getBounds() { //return hitbox normal attack
+	public Rectangle getBounds() {
 		return new Rectangle((int)this.x, (int)this.y, (int)this.width, (int)this.height);
 	}
 	
-	private void createPochita() { //inisialisasi NA imageView
+	private void createPochita() {
 		pochitaIcon.setFitWidth(this.width);
 		pochitaIcon.setFitHeight(this.height);
 		pochitaIcon.setLayoutX(this.x);
