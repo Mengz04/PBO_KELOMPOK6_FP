85c9abdb46c3cfa499f134ae360b0e5df5600714
Muhammad Rafi Insan Fillah <78022264+Mengz04@users.noreply.github.com>
2022-12-17 09:11:33 +0700

    Update GameWindow.java

--- src/application/GameWindow.java
+++ src/application/GameWindow.java
@@ -1,21 +1,12 @@
 package application;
 
-import java.awt.Button;
-import java.util.ArrayList;
-import java.util.List;
-
 import javafx.animation.AnimationTimer;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.SubScene;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyCombination;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /*
@@ -34,20 +25,17 @@ public class GameWindow{
 	public static final int BGWIDTH = 5760;
 	
 	public static final int CHAR_SIZE = 72;
-	private static final int MENU_BUTTONS_START_X = 300-85;
-	private static final int MENU_BUTTONS_START_Y = 100;
 	
 	private AnchorPane gamePane;
 	private Scene gameScene;
 	private Stage gameStage;
 	private GameObject tempPlayer = null;
 	private GameObject tempMob = null;
+	private KeyInput inputKey;
 	
-	private Stage mainStage;
-	private GameSubScene subScene;
+	private Rectangle EXPBar;
 	
 	private Boolean isRunning = false;
-	private Boolean isSubSceneHidden = true;
 	
 	private AnimationTimer gameTimer;
 	private Handler handler;
@@ -55,30 +43,22 @@ public class GameWindow{
 	private int NAcooldownBUF = 100;
 	private int NAcooldown = NAcooldownBUF;
 	private int level = 0;
-	private int EXPCap = 100;
 	private int spawnInterval = 600;
 	private int spawner = spawnInterval;
-	
-	List<MenuButton> subSceneButtons;
-	
+	private int gacha;
 	
 	public GameWindow() { //new game base instruction
-		initializeCanvas();
-	}
-	
-	public void createNewGame(Stage mainStage) {
-		this.mainStage = mainStage;
-		this.mainStage.hide();
 		isRunning = true;
+		initializeCanvas();
 		createObjects();
 		createGameLoop();
-		gameStage.show();
 	}
 	
 	private void createObjects() { //spawn objects
 		handler = new Handler();
+		inputKey = new KeyInput(gameScene);
 		handler.addObject(new Block(0, 0, ID.Block, 135, 97,gamePane,handler, "GreenContainerHorizontal"));
-		tempPlayer = new Player((WIDTH/2)-(CHAR_SIZE/2), (HEIGHT/2)-(CHAR_SIZE/2), ID.Player, handler, gamePane, gameScene);
+		tempPlayer = new Player((WIDTH/2)-(CHAR_SIZE/2), (HEIGHT/2)-(CHAR_SIZE/2), ID.Player, handler, gamePane, gameScene, inputKey);
 		handler.addObject(tempPlayer);
 		cam = new Camera(0,0, handler);
 	}
@@ -96,10 +76,7 @@ public class GameWindow{
 		gameScene.setOnMouseClicked(mouseHandler);
 		gameStage = new Stage();
 		gameStage.setScene(gameScene);
-		gameStage.setFullScreen(true);
-		gameStage.setResizable(false);
-		gameStage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
-		
+		Rectangle EXPBG = new Rectangle(0, 1055, 1920, 25);
 	}
 	
 	private void createGameLoop() {
@@ -107,24 +84,96 @@ public class GameWindow{
 
 			@Override
 			public void handle(long now) { //Game tick method
-				checkIfPlayerDied();
-				checkLevel();
-				spawnMob();
-				if(NAcooldown>0)NAcooldown--;
-				gamePane.setLayoutX(gamePane.getLayoutX()+cam.getX());
-				gamePane.setLayoutY(gamePane.getLayoutY()+cam.getY());
-				handler.move();
-				if(!handler.removeTask.isEmpty()) {
-					if(handler.removeTask.peek().getId()== ID.Zombie) {
-						tempMob = handler.removeTask.peek();
-						handler.addObject(new BloodEXP(tempMob.x+tempMob.width/2, tempMob.y+tempMob.height, ID.BloodEXP, gamePane, handler));
-					}
-					handler.removeObject(handler.removeTask.poll());
-					//System.out.println("mark");
+				if(tempPlayer.HP <= 0) { // player mati
+					isRunning = false;
 				}
-				cam.camTick();
-				gamePane.setLayoutX(gamePane.getLayoutX()-cam.getX());
-				gamePane.setLayoutY(gamePane.getLayoutY()-cam.getY());
+				if(isRunning) {
+					if(tempPlayer.getEXPCap() == tempPlayer.getEXP()) { //level up condition
+						tempPlayer.addEXPCap(50);
+						level++;
+						tempPlayer.setEXP(0);
+						if(spawnInterval>190) { //cap minimum spawnInterval = 200
+							spawnInterval -=10;
+						}
+					}
+					if(spawner<=0) { //spawn mob generator
+						int position = getRandomNumber(1,4);
+						//kuadran adjustment
+						if(position == 1) { //kuadran 1 thd player
+							if(tempPlayer.x<=2094 && tempPlayer.y>=-105) {position = 1;}
+							else if(tempPlayer.x<=2094) {position = 4;}
+							else if(tempPlayer.y>=-105) {position = 2;}
+							else {position = 3;}
+						}
+						if(position == 2) { //kuadran 2 thd player
+							if(tempPlayer.x>=-250 && tempPlayer.y>=-105) {position = 2;}
+							else if(tempPlayer.x>=-250) {position = 3;}
+							else if(tempPlayer.y>=-105) {position = 1;}
+							else {position = 4;}
+						}
+						if(position == 3) { //kuadran 2 thd player
+							if(tempPlayer.x>=-250 && tempPlayer.y<=1215) {position = 3;}
+							else if(tempPlayer.x>=-250) {position = 2;}
+							else if(tempPlayer.y<=1215) {position = 4;}
+							else {position = 1;}
+						}
+						if(position == 4) { //kuadran 1 thd player
+							if(tempPlayer.x<=2094 && tempPlayer.y<=1215) {position = 4;}
+							else if(tempPlayer.x<=2094) {position = 1;}
+							else if(tempPlayer.y<=1215) {position = 3;}
+							else {position = 2;}
+						}
+						//spawn correct kuadran
+						if(position == 1) { //kuadran 1 thd player
+							handler.addObject(new Mob(tempPlayer.getX()+(WIDTH/2+100),
+								tempPlayer.getY()-(HEIGHT/2+100), ID.Zombie, gamePane, handler));
+						}
+						if(position == 2) { //kuadran 2 thd player
+								handler.addObject(new Mob(tempPlayer.getX()-(WIDTH/2+100),
+									tempPlayer.getY()-(HEIGHT/2+100), ID.Zombie, gamePane, handler));
+						}
+						if(position == 3) { //kuadran 3 thd player
+							handler.addObject(new Mob(tempPlayer.getX()-(WIDTH/2+100),
+									tempPlayer.getY()+(HEIGHT/2+100), ID.Zombie, gamePane, handler));
+						}
+						if(position == 4){ //kuadran 4 thd player
+							handler.addObject(new Mob(tempPlayer.getX()+(WIDTH/2+100),
+									tempPlayer.getY()+(HEIGHT/2+100), ID.Zombie, gamePane, handler));
+						}
+							
+						spawner = spawnInterval;
+					}
+					
+					spawner--;
+					if(NAcooldown>0)NAcooldown--;
+					gamePane.setLayoutX(gamePane.getLayoutX()+cam.getX());
+					gamePane.setLayoutY(gamePane.getLayoutY()+cam.getY());
+					while(!handler.removeTask.isEmpty()) {
+						if(handler.removeTask.peek().getId()== ID.Zombie) {
+							tempMob = handler.removeTask.peek();
+							gacha = getRandomNumber(1,3);
+							if(gacha == 1) {
+								gacha = getRandomNumber(1,5);
+								switch (gacha) {
+									case 1: handler.addObjTask.add(new DropItem(tempMob.x+tempMob.width/2 -25, tempMob.y+tempMob.height/2, ID.Drop, gamePane, handler, "health")); break;
+									case 2: handler.addObjTask.add(new DropItem(tempMob.x+tempMob.width/2 -25, tempMob.y+tempMob.height/2, ID.Drop, gamePane, handler, "speed"));break;
+									case 3: handler.addObjTask.add(new DropItem(tempMob.x+tempMob.width/2 -25, tempMob.y+tempMob.height/2, ID.Drop, gamePane, handler, "invincibility"));break;
+									case 4: handler.addObjTask.add(new DropItem(tempMob.x+tempMob.width/2 -25, tempMob.y+tempMob.height/2, ID.Drop, gamePane, handler, "GDevil"));break;
+									default: handler.addObjTask.add(new DropItem(tempMob.x+tempMob.width/2 -25, tempMob.y+tempMob.height/2, ID.Drop, gamePane, handler, "BDevil"));
+								}
+							}
+							handler.addObject(new BloodEXP(tempMob.x+tempMob.width/2, tempMob.y+tempMob.height, ID.BloodEXP, gamePane, handler));
+						}
+						handler.removeObject(handler.removeTask.poll());
+					}
+					while(!handler.addObjTask.isEmpty()) {
+						handler.addObject(handler.addObjTask.poll());
+					}
+					handler.move();
+					cam.camTick();
+					gamePane.setLayoutX(gamePane.getLayoutX()-cam.getX());
+					gamePane.setLayoutY(gamePane.getLayoutY()-cam.getY());
+				}
 			}
 		};
 
@@ -160,110 +209,9 @@ public class GameWindow{
         }
      
     };
-    
+
 	public Stage getMainStage() {
 		return gameStage;
 	}
-
-	private void checkIfPlayerDied() {
-		if(tempPlayer.HP <= 0) { // player mati
-			isRunning = false;
-			gameTimer.stop();
-			createSubScene();
-			showSubScene(subScene);
-		}
-	}
-	
-	private void spawnMob() {
-		//handler.addObject(new Mob(tempPlayer.getX()+getRandomNumber(WIDTH/2+100, WIDTH/2+500),tempPlayer.getY()-getRandomNumber(HEIGHT/2+100, HEIGHT/2+500), ID.Zombie, gamePane, handler));
-		if(spawner<=0) { //spawn mob generator
-			int position = getRandomNumber(1,4);
-			if(position == 1) { //kuadran 1 thd player
-				handler.addObject(new Mob(tempPlayer.getX()+(WIDTH/2+100),
-						tempPlayer.getY()-(HEIGHT/2+100), ID.Zombie, gamePane, handler));
-			}
-			if(position == 2) { //kuadran 2 thd player
-				handler.addObject(new Mob(tempPlayer.getX()-(WIDTH/2+100),
-						tempPlayer.getY()-(HEIGHT/2+100), ID.Zombie, gamePane, handler));
-			}
-			if(position == 3) { //kuadran 3 thd player
-				handler.addObject(new Mob(tempPlayer.getX()-(WIDTH/2+100),
-						tempPlayer.getY()+(HEIGHT/2+100), ID.Zombie, gamePane, handler));
-			}
-			if(position == 3){ //kuadran 4 thd player
-				handler.addObject(new Mob(tempPlayer.getX()+(WIDTH/2+100),
-						tempPlayer.getY()-(HEIGHT/2+100), ID.Zombie, gamePane, handler));
-			}
-			spawner = spawnInterval;
-		}
 		
-		spawner--;
-		//System.out.println(spawner);
-	}
-	
-	private void checkLevel() {
-		//System.out.println(tempPlayer.HP);
-		if(EXPCap == tempPlayer.getEXP()) { //level up condition
-			EXPCap += 50;
-			level++;
-			tempPlayer.setEXP(0);
-			if(spawnInterval>190) { //cap minimum spawnInterval = 200
-				spawnInterval -=10;
-			}
-		}
-	}
-	
-	private void createSubScene() {
-		subScene = new GameSubScene((int)tempPlayer.getX()-300+48, (int)tempPlayer.getY()+540);
-		gamePane.getChildren().add(subScene);
-		createSubSceneButtons();
-	}
-	
-	public void showSubScene(GameSubScene subScene) {
-		subScene.moveSubscene();
-	}
-	
-	private void addSubSceneButtons(MenuButton button) {
-		button.setLayoutX(MENU_BUTTONS_START_X);
-		button.setLayoutY(MENU_BUTTONS_START_Y + subSceneButtons.size() * 100);
-		subSceneButtons.add(button);
-		subScene.getPane().getChildren().add(button);
-	}
-	
-	private void createSubSceneButtons() {
-		subSceneButtons = new ArrayList<>();
-		createRestartButton();
-		createExitToMainMenuButton();
-	}
-	
-	private void createRestartButton() {
-		MenuButton restartButton = new MenuButton("RESTART");
-		addSubSceneButtons(restartButton);
-		
-		restartButton.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent arg0) {
-				gameStage.close();
-				GameWindow game = new GameWindow();
-				game.createNewGame(mainStage);
-			}
-		});
-	}
-	
-	private void createExitToMainMenuButton() {
-		MenuButton exitToMainMenuButton = new MenuButton("MAIN MENU");
-		addSubSceneButtons(exitToMainMenuButton);
-		
-		exitToMainMenuButton.setOnAction(new EventHandler<ActionEvent>() {
-			
-			@Override
-			public void handle(ActionEvent arg0) {
-				gameStage.close();
-				mainStage.show();
-			}
-		});
-	}
-	
-	
 }
